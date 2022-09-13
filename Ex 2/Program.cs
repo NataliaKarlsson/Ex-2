61adf5121d68ee0256f5917c63cbbcbb470f1cf9
NataliaKarlsson <112874590+NataliaKarlsson@users.noreply.github.com>
2022-09-13 14:05:20 +0200

    Add project files.

---
+++ Ex 2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex_2
+{
+    internal class Program
+    {
+        
+        
+            static void Main(string[] args)
+            {
+
+
+                Console.Write("Enter a number between 1 to 10: ");
+                string? str = Console.ReadLine();
+                int num = Convert.ToInt32(str);
+                bool s = (num >= 1 && num <= 10);
+
+                while (s == false)
+                {
+                    Console.Write("Enter a number between 1 to 10: ");
+                    str = Console.ReadLine();
+                    num = Convert.ToInt32(str);
+                    s = (num >= 1 && num <= 10);
+
+                }
+
+
+
+
+
+            }
+        
+    }
+}
+    
